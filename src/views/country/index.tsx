f84366b28d2863b7e1be38416a2ed12ceb7b1b9f
joe <253482219@qq.com>
2023-08-16 16:16:01 +0800

    fix: deploy

--- src/views/country/index.tsx
+++ src/views/country/index.tsx
@@ -2,8 +2,6 @@ import Layout from '@/components/layout';
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router'
 // import CountryMenu from '../studyabroad/countryMenu';
-import American from './American'
-import Hongkong from './Hongkong';
 import CountryLayout from './CountryLayout';
 import ModuleWrapper from '@/components/moduleWrapper';
 import CountryTitle from './components/CountryTitle';
